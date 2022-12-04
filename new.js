8ad14d2465ea48eb700a46af033b23de2d7ca003
RohanJung <rohanjungtmg@gmail.com>
2022-12-04 07:31:46 +0545

    updating the files

--- new.js
+++ new.js
@@ -1,61 +1,80 @@
 
-//function
-// function getDate(){
+// //function
+// // function getDate(){
 
     
-// 	var date = new Date();
+// // 	var date = new Date();
 	
-//     let day = date.getDate();
-//     let month = date.getMonth();
-// document.getElementById("day").innerHTML = day+"-"+month;
-//document.getElementById("day").innerHTML = month;
+// //     let day = date.getDate();
+// //     let month = date.getMonth();
+// // document.getElementById("day").innerHTML = day+"-"+month;
+// //document.getElementById("day").innerHTML = month;
+// // }
+
+// for(i=0;i<100;i++){
+
+//     document.write("Hello I am " + i + "<br>");
+// }
+// // var x=1;
+// // while(x<=100){
+// //     document.write("Hello I am " + x + "<br>");
+// //     x++;
+// // }
+// // y=0;
+// // do{
+// //     document.write("Hello I am " + y + "<br>");
+// //     y++;
+// // }
+// // while(y<100);
+
+
+// // var students = ["ram", "hari","sita"];
+// // for(z=0;Z<=2;z++){
+// //     document.write(students[0])
+// // }
+
+
+// //Objects
+
+// // var ram = new Object();
+// // ram.name = "ram silwal"
+// // ram.age = 45;
+// // function sayHi(){
+// //     document.write('Hi');
+
+// // }
+
+// // var ramu = {
+// //     name:"ram",
+// //     age:45,
+// //     hi:sayHi
+
+// // }
+// // // document.write(ramu.hi)
+// // // document.write(ramu.name)
+
+// // //Constructor
+
+// // function person(name,age){
+// //     this.name = name
+// //     this.age = age
+// // }
+
+// for in
+
+// var sub = ['Meth', 'Social', 'Science']
+// for (x in sub){
+//     document.write(sub[x])
 // }
 
-for(i=0;i<100;i++){
 
-    document.write("Hello I am " + i + "<br>");
+function calcSum(){
+    let x = document.getElementById('a').value
+let y = document.getElementById('b').value
+
+let valA = parseInt(x);
+let valB = parseInt(y);
+let sum = valA + valB;
+let summer = document.getElementById('sum');
+summer.innerHTML= sum;
 }
-// var x=1;
-// while(x<=100){
-//     document.write("Hello I am " + x + "<br>");
-//     x++;
-// }
-// y=0;
-// do{
-//     document.write("Hello I am " + y + "<br>");
-//     y++;
-// }
-// while(y<100);
-
-
-// var students = ["ram", "hari","sita"];
-// for(z=0;Z<=2;z++){
-//     document.write(students[0])
-// }
-
-
-//Objects
-
-// var ram = new Object();
-// ram.name = "ram silwal"
-// ram.age = 45;
-// function sayHi(){
-//     document.write('Hi');
-
-// }
-
-// var ramu = {
-//     name:"ram",
-//     age:45,
-//     hi:sayHi
-
-// }
-// // document.write(ramu.hi)
-// // document.write(ramu.name)
-
-// //Constructor
-
-// function person(name,age){
-//     this.name = name
-//     this.age = age
-// }
